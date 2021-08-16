0c3997a1ef1fc31d8f149cc9a7e24032c7111f9f
Dorien Reynders <dorien.reynders@hotmail.com>
2021-08-16 22:11:14 +0200

    add dotenv - protect my secret data in .env file

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,2 +1,9 @@
 class ToolsController < ApplicationController
+  def index
+    if params[:query].present?
+      @tools = Tool.where(title: params[:query])
+    else
+      @tools = Tool.all
+    end
+  end
 end
