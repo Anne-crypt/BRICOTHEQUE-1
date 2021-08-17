2ce47ad8f123787eb1c16179306cdc9c46d9dc21
rahim pauillac <pauillacrahim@gmail.com>
2021-08-17 10:14:51 +0200

    creating tool card

--- db/schema.rb
+++ db/schema.rb
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2021_08_16_144533) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
+    t.string "name", null: false
     t.string "category"
     t.text "description"
     t.index ["user_id"], name: "index_tools_on_user_id"
